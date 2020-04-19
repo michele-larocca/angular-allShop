36f44063016df7b92a044306a3285f66ba9d93f9
Michele Larocca <michele.larocca@softeco.it>
2020-04-19 18:16:23 +0200

    add route to Welcome Component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+import { WelcomeComponent } from './welcome/welcome.component';
 
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
+  {path: 'welcome', component: WelcomeComponent},
   {path: '**', component: PageNotFoundComponent}
 ];
 
