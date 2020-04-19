7023e5867c32acf210d3aba202b3d06ba61b17b0
Michele Larocca <michele.larocca@softeco.it>
2020-04-19 18:10:16 +0200

    add route for PageNotFound when the url is wrong

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 const routes: Routes = [
   {path: '', component: LoginComponent},
   {path: 'login', component: LoginComponent},
+  {path: '**', component: PageNotFoundComponent}
 ];
 
 @NgModule({
