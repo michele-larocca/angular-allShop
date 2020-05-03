a6563a40d0b2675597208b51f16fc95e598dc6dc
Michele Larocca <michele.larocca@softeco.it>
2020-05-03 19:54:15 +0200

    add dynamic filtered by description

--- src/app/articoli/articoli.component.ts
+++ src/app/articoli/articoli.component.ts
@@ -32,12 +32,34 @@ export class ArticoliComponent implements OnInit {
   constructor(private articoliData: ArticoliDataService) { }
 
   ngOnInit(): void {
+    this.refreshTable();
+  }
+
+  refreshTable(){
     this.articoliData.getArticoli(this.filter).subscribe(
       (response) => {
         this.items = response;
         this.totalItems = this.items ? this.items.length : 0;
+
+        if(this.pageSelected > 1 && this.totalItems <= ((this.pageSelected-1) * this.rowDisplayed)){
+          this.pageSelected = 1;
+        }
+      },
+      error => {
+        const { error : { codice = undefined} = {}} = error || {};
+        
+        if(codice == 404)
+          this.resetTable();
+        else 
+          console.error("Error Articoli.getArticoli ", error);
       }
     )
+  }
+
+  resetTable(){
+    this.items = [];
+    this.totalItems = 0;
+    this.pageSelected = 1;
   }
 
 }
